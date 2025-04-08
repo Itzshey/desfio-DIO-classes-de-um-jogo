b282ae70e5f0a3d4cb905cbc8be15ea023bb4099
Sofia Helene <sofiahelene1403@gmail.com>
2025-04-08 16:17:52 -0300

    segunda versão

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("teste")
+class heroi {
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atack(){
+        if(this.tipo === "mago"){
+            console.log("O heroi " + this.nome + " de tipo " + this.tipo + " usou magia")
+        } else if (this.tipo === "monge"){
+            console.log("O heroi " + this.nome + " de tipo " + this.tipo + " usou artes marciais")
+        } else if (this.tipo === "guerreiro"){
+            console.log("O heroi " + this.nome + " de tipo " + this.tipo + " usou espada")
+        } else if (this.tipo === "ninja"){
+            console.log("O heroi " + this.nome + " de tipo " + this.tipo + " usou shuriken")
+        }
+    }
+}
+
+const nome = ["Frederico", "Jefferson Caminhões", "Tonhão"]
+const idade = [13, 26, 45]
+const classe = ["mago","monge","guerreiro"]
+
+for(i = 0; i <= nome.length; i++){
+    const novoHeroi = new heroi (nome[i], idade[i], classe[i]);
+    novoHeroi.atack()
+}
+
